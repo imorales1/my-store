f0ae02fe97c531ad1e5312e72e89a2f05a8a77a3
Ismar E. Morales <imorales@sandiego.com.gt>
2022-06-27 23:09:32 -0600

    Intruduccion al Event Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'my-store';
+  name = 'Alejandro';
+  age = 25;
+  img = 'https://www.peru.travel/Contenido/AcercaDePeru/Imagen/es/1/0.0/Principal/Machu%20Picchu.jpg';
+  btnDisabled = true;
+  person = {
+    name: 'Alejandro',
+    age: 25,
+    avatar: 'https://www.peru.travel/Contenido/AcercaDePeru/Imagen/es/1/0.0/Principal/Machu%20Picchu.jpg'
+  }
+
+  toggleButton() {
+    /*this.btnDisabled = false;  */
+    this.btnDisabled = !this.btnDisabled;
+  }
+
+  increaseAge() {
+    this.person.age +=1;
+  }
 }
