7d97cfe9ed08051b6d3cd6b08fdf3319203a15ce
Ismar E. Morales <imorales@sandiego.com.gt>
2022-06-29 22:53:28 -0600

    Finalizando modulo comunicación de datos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,7 @@ export class AppComponent {
   age = 25;
   img = 'https://www.peru.travel/Contenido/AcercaDePeru/Imagen/es/1/0.0/Principal/Machu%20Picchu.jpg';
   btnDisabled = true;
+
   person = {
     name: 'Alejandro',
     age: 25,
@@ -23,5 +24,15 @@ export class AppComponent {
 
   increaseAge() {
     this.person.age +=1;
+  }
+
+  onScroll(event: Event) {
+    const element = event.target as HTMLElement;
+    console.log(element.scrollTop);
+  }
+
+  changeName(event: Event) {
+    const element = event.target as HTMLInputElement;
+    this.person.name = element.value;
   }
 }
